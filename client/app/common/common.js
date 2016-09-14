8a9af1d56fa9ab43dd3cd4ad44635dff1308c1c0
Daniel Pinto <daniel.pinto@inw-tech.com>
2016-09-14 18:55:42 +0100

    Communicating with an API

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,8 +1,12 @@
 import angular from 'angular';
 import Navbar from './navbar/navbar';
+import PokeapiClient from './pokeapiClient/pokeapiClient';
+import ThreeNumbersAlways from './threeNumbersAlways/threeNumbersAlways';
 
 let commonModule = angular.module('app.common', [
   Navbar,
+  PokeapiClient,
+  ThreeNumbersAlways
 ])
 
 .name;
